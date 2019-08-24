209432ebd081131bc77136b0a0ce4a65f659f125
Clint Batte <clint.batte@gmail.com>
2019-08-24 22:07:46 +0000

    adding the about page and more

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,20 +1,29 @@
+# frozen_string_literal: true
+
 class QuotesController < ApplicationController
   def index
-    @quote = Quote.order("RANDOM()").first
+    @quote = Quote.order('RANDOM()').first
   end
+
   def new
     @quote = Quote.new
   end
 
   def create
-    Quote.create(quote_params)
+    @quote = Quote.create(quote_params)
+    if @quote.invalid?
+      flash[:error] = '<strong>Could not save</strong> the data you entered is invlaid.'
+    end
     redirect_to root_path
   end
 
-private
+  def about
+    
+  end
+
+  private
 
   def quote_params
     params.require(:quote).permit(:saying, :author)
   end
-
 end
